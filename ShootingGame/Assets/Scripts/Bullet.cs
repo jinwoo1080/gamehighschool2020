df3cad0ba1b36fdfc2a9342c458b1630fd89483a
jinwoo1080 <jinwoo1080@naver.com>
2020-07-20 21:12:04 +0900

    1일 슈팅

---
+++ ShootingGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float m_Speed = 2f;
+
+    private void Update()
+    {
+        Vector3 movemont = transform.up * m_Speed * Time.deltaTime;
+        transform.position += movemont;
+    }
+}
---
+++ ShootingGame/Assets/Scripts/ShipController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float m_Speed = 5f;
+
+
+    private void Update()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector2(xAxis, yAxis) * m_Speed);
+    }
+}
