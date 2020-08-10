9140ca96d567aafd35277dbeaa8231b04d8b8c60
jinwoo1080 <jinwoo1080@naver.com>
2020-08-10 21:02:24 +0900

    1dlf

---
+++ ang1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+    public void OnPointerDown(BaseEventData eventData)
+    {
+        Debug.Log("오브젝트 클릭");
+        //throw new System.NotImplementedException();
+    }
+}
